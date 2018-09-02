4e3d2747378f82ca3c07ae3ee9f62d8c43c4165d
Jan-Nilas Gerth <janx1644@edu.eal.dk>
2018-09-02 21:28:40 +0200

    clean css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+let cleanCSS = require('gulp-clean-css');
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require("browser-sync");
 var scssPath = "src/styles/**/*.scss";
@@ -9,12 +10,12 @@ gulp.task('sass', function () {
     return gulp.src(scssPath)
     .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('build/styles/css'))
+      .pipe(cleanCSS({compatibility: 'ie8'}))
       .pipe(autoprefixer({
           browsers: ['last 2 versions'],
           cascade: false
       }))
-      .pipe(sourcemaps.write())
+      .pipe(cleanCSS())
       .pipe(gulp.dest('build/styles/css'));
   });
    
@@ -22,14 +23,6 @@ gulp.task('sass', function () {
     gulp.watch(scssPath, ['sass']);
   });
 
-gulp.task('autoprefixer', () =>
-    gulp.src('build/styles/css/main.css')
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))
-        .pipe(gulp.dest('dist'))
-);
 
 gulp.task("serve", ["sass"], function() {
     browserSync.init({
